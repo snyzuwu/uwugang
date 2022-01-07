93eb8dc2f07aef09b3929a50d850b66bf22656f4
snyzuwu <nysgodx@gmail.com>
2022-01-07 12:36:43 -0600

    exo fini

---
+++ j02/ex00/ft_print_alphabet.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+int ft_putchar(char c)
+{
+    write(1,&c,1);
+    return (0);
+}
+
+void ft_print_alphabet(void)
+{
+    char c;
+    
+    c = 'a';
+    while (c <= 'z')
+    {
+        ft_putchar(c);
+        c = c + 1;
+    }
+}
+
+int  main()
+{
+    ft_print_alphabet();
+    ft_putchar('\n');
+    return (0);
+}
